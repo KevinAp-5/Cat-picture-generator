327ffdb23f706e8701c3901e4d815fbff731da4f
KevinAp-5 <keven.clash3@gmail.com>
2020-11-02 22:34:33 -0300

    while if the request != 200. request counter to quit is > 5

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ else:
     for x in path:
         os.remove(f'{os.getcwd()}/cats/{x}')
 
-api = 'https://api.thecatapi.com/v1/images/search'
-image_url = requests.get(api, headers={'User-Agent': 'python'})
 
-if image_url.status_code != 200:
-    print('Error:', image_url.status_code)
-    exit()
+api = 'https://api.thecatapi.com/v1/images/search'
+counter = 0
+while True:
+    image_url = requests.get(api, headers={'User-Agent': 'python'})
+    if image_url.status_code != 200:
+        print(f'Error: {image_url.status_code}\nTrying again...')
+        if counter == 5:
+            print('Error trying to request...\nVerify you connection.')
+            exit()
+        counter += 1
+        continue
+    else:
+        break
+
 
 url = image_url.json()
 name = ''
-
 for x, y in url[0].items():
     if x == 'url':
         name = y.split('/')[-1]
@@ -33,5 +41,5 @@ for x, y in url[0].items():
             except Exception:
                 raise
             else:
-                os.system(f'xdg-open cats/{name}')  # open the image
+                os.system(f'sensible-browser cats/{name}')  # open the image
 
