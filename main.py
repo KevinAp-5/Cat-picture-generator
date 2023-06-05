f514661f66b9b367e575a8799de68f76b0401fa7
KevinAp-5 <keven.clash3@gmail.com>
2023-06-04 23:59:54 -0300

    Cleancode

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 import requests
 from PIL import Image
-from time import sleep
-from platform import system
 from contextlib import suppress
+from time import sleep
+
 
 def folder_path():
     return f'{os.getcwd()}/cats/'
+
 
 def create_folder():
     if os.path.isdir(folder_path()) is False:
@@ -15,9 +16,11 @@ def create_folder():
     else:
         return False
 
+
 def remove_photos():
     for photo in os.listdir(folder_path()):
         os.remove(f'{folder_path()}{photo}')
+
 
 def check_api_status(request):
     if request.status_code != 200:
@@ -25,29 +28,39 @@ def check_api_status(request):
     else:
         print('Connection with the API was successful.')
 
+
 def get_url():
     api_url = requests.get('https://api.thecatapi.com/v1/images/search')
     return api_url.json()[0].get('url')
+
 
 def request_api(image_url):
     with suppress():
         return requests.get(image_url)
 
+
 def save_picture(foto, file_name):
     with open(f'{folder_path()}{file_name}', 'wb') as request_:
         request_.write(foto.content)
+
 
 def open_image(file_name):
     photo = Image.open(f'cats/{file_name}')
     photo.show()
 
+
 if __name__ == '__main__':
     if create_folder() is False:
         remove_photos()
 
-    file_name = get_url().split('/')[-1]
     image_url = get_url()
+    file_name = image_url.split('/')[-1]
+    print('Verificando conexão')
     check_api_status(request_api(image_url))
-    save_picture(request_api(image_url), file_name)
-    open_image(file_name)
 
+    print('Salvando a foto.')
+    save_picture(request_api(image_url), file_name)
+    sleep(0.3)
+
+    print('Abrindo foto.')
+    open_image(file_name)
