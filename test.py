77b7aee7b5c86f9926031659da64d21ca1b77f40
KevinAp-5 <keven.clash3@gmail.com>
2023-06-11 00:38:54 -0300

    create_folder
    Check if the .cats folder exists, if doesn't it will create the dir and add true to folder_exist

--- test.py
+++ test.py
@@ -10,3 +10,10 @@ class FolderManip():
         self.folder_path = f'{os.getcwd()}/.cats/'
         self.folder_exist = False
         self.create_folder()
+
+    def create_folder(self):
+        if os.path.isdir(self.folder_path) is False:
+            os.mkdir(self.folder_path)
+            self.folder_exist = True
+        else:
+            self.folder_exist = True
