963dfaf9dc7b4d40fabe6a7ed9c36ede13c3d119
KevinAp-5 <keven.clash3@gmail.com>
2023-06-11 00:34:34 -0300

    Class FolderManip and constructor

--- test.py
+++ test.py
@@ -3,3 +3,10 @@ import requests
 from PIL import Image
 from contextlib import suppress
 from time import sleep
+
+
+class FolderManip():
+    def __init__(self):
+        self.folder_path = f'{os.getcwd()}/.cats/'
+        self.folder_exist = False
+        self.create_folder()
