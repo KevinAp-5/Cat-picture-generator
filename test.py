1f75e51d708a7b5765e559ca241b08be9be36e00
KevinAp-5 <keven.clash3@gmail.com>
2023-06-11 00:49:40 -0300

    remove the old photos of cats

--- test.py
+++ test.py
@@ -17,3 +17,8 @@ class FolderManip():
         else:
             os.mkdir(self.folder_path)
             self.folder_exist = True
+
+    def remove_photos(self):
+        if self.folder_exist is True:
+            for photo in os.listdir(self.folder_path):
+                os.remove(f'{self.folder_path}{photo}')
