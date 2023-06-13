c8c0bc21d380a3afcf8212507c6fe0dd30309b52
KevinAp-5 <keven.clash3@gmail.com>
2023-06-13 03:49:59 -0300

    switched to OOP

--- main.py
+++ main.py
@@ -5,62 +5,77 @@ from contextlib import suppress
 from time import sleep
 
 
-def folder_path():
-    return f'{os.getcwd()}/.cats/'
+class FolderManip():
+    def __init__(self):
+        self.folder_path = f'{os.getcwd()}/.cats/'
+        self.folder_exist = False
+        self.create_folder()
+
+    def create_folder(self):
+        if os.path.isdir(self.folder_path):
+            self.folder_exist = True
+        else:
+            os.mkdir(self.folder_path)
+            self.folder_exist = True
+
+    def remove_photos(self):
+        if self.folder_exist is True:
+            for photo in os.listdir(self.folder_path):
+                os.remove(f'{self.folder_path}{photo}')
 
 
-def create_folder():
-    if os.path.isdir(folder_path()) is False:
-        os.mkdir(folder_path())
-        return True
-    else:
-        return False
+class Api():
+    def __init__(self):
+        self.api = 'https://api.thecatapi.com/v1/images/search'
+
+    def check_api_status(self, request):
+        if request.status_code != 200:
+            print(f'Error: {request.status_code}. Verify your connection')
+            exit()
+        else:
+            print('Connection was successful.')
+
+    def get_image_url(self):
+        api_url = requests.get(self.api)
+        print('Connecting to API...')
+        sleep(0.5)
+        self.check_api_status(api_url)
+        return api_url.json()[0].get('url')
+
+    def request_image_url(self, image_url):
+        with suppress():
+            image = requests.get(image_url)
+            print('Collecting the photo...')
+            sleep(0.5)
+            self.check_api_status(image)
+            return image
 
 
-def remove_photos():
-    for photo in os.listdir(folder_path()):
-        os.remove(f'{folder_path()}{photo}')
+class Photo():
+    def __init__(self, api, foldermanip):
+        self.api = api()
+        self.foldermanip = foldermanip()
+        self.image_url = self.api.get_image_url()
+        self.image_name = self.image_url.split('/')[-1]
 
+    def image_path(self):
+        return f'{self.foldermanip.folder_path}{self.image_name}'
 
-def check_api_status(request):
-    if request.status_code != 200:
-        print(f'Error: {image_url.status_code}. Verify your connection')
-    else:
-        print('Connection with the API was successful.')
+    def get_photo(self):
+        return self.api.request_image_url(self.image_url)
 
+    def save_picture(self):
+        with open(f'{self.image_path()}', 'wb') as file:
+            file.write(self.get_photo().content)
 
-def get_url():
-    api_url = requests.get('https://api.thecatapi.com/v1/images/search')
-    return api_url.json()[0].get('url')
-
-
-def request_api(image_url):
-    with suppress():
-        return requests.get(image_url)
-
-
-def save_picture(foto, file_name):
-    with open(f'{folder_path()}{file_name}', 'wb') as request_:
-        request_.write(foto.content)
-
-
-def open_image(file_name):
-    photo = Image.open(f'.cats/{file_name}')
-    photo.show()
+    def open_image(self):
+        photo = Image.open(self.image_path())
+        print('Opening the photo!')
+        photo.show()
 
 
 if __name__ == '__main__':
-    if create_folder() is False:
-        remove_photos()
-
-    image_url = get_url()
-    file_name = image_url.split('/')[-1]
-    print('Verificando conexão')
-    check_api_status(request_api(image_url))
-
-    print('Salvando a foto.')
-    save_picture(request_api(image_url), file_name)
-    sleep(0.3)
-
-    print('Abrindo foto.')
-    open_image(file_name)
+    x = Photo(Api, FolderManip)
+    x.foldermanip.remove_photos()
+    x.save_picture()
+    x.open_image()
